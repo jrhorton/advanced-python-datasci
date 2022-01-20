1d64d79b13ca0763c39bd9de261a2bc9ab609f20
jrhorton <57638737+jrhorton@users.noreply.github.com>
2022-01-20 09:44:17 -0500

    Day 4 mid morning commit

--- Notebooks/my_module.py
+++ Notebooks/my_module.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
+
 def get_features_and_target(csv_file, target_col):
     '''Split a CSV into a DF of numeric features and a target column.'''
     
@@ -17,6 +18,21 @@ def get_features_and_target(csv_file, target_col):
     
     return (features, target)
 
+"""
+def get_features_and_target(csv_file, target_col):
+    '''Split a CSV into a DF of numeric features and a target column.'''
+    
+    adult_census = pd.read_csv(csv_file)
+    
+    raw_features = adult_census.drop(columns=target_col)
+    numeric_features = raw_features.select_dtypes(np.number)
+    feature_cols = numeric_features.columns.values
+
+    features = adult_census[feature_cols]
+    target = adult_census[target_col]
+    
+    return (features, target)
+"""
 def make_preprocessor(features, categorical_preprocessor=None, numeric_preprocessor=None):
     '''Create a column transformer that applies sensible preprocessing procedures.'''
     
